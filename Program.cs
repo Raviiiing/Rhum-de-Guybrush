a27f4668b80466a20a65eb62557860cbcd5c5739
votre-nom <votre-mail@entreprise.com>
2021-04-09 18:55:29 +0200

    Modification du chemin

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace PROJET_CSHARP
     {
         static void Main(string[] args)
         {
-            CarteDecodee laCarte = new CarteDecodee(@"C:\Users\Yann\OneDrive\IUT\Semestre 2\C#\PROJET\Projet\PROJET_CSHARP\Cartes\Scabb.clair");
+            CarteDecodee laCarte = new CarteDecodee(@"../../../Cartes/Scabb.clair");
             laCarte.Affiche();
         }
     }
