140015424fd13f7b08b631edc8fad000d639fc98
votre-nom <votre-mail@entreprise.com>
2021-04-09 16:02:09 +0200

    Update main

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace PROJET_CSHARP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test Paul 4");
+            string coucou = "test";
         }
     }
 }
