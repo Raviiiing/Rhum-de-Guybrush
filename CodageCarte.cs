0fae07d670e0c07931a48e4924674be005eec97b
votre-nom <votre-mail@entreprise.com>
2021-04-09 18:18:00 +0200

    test

---
+++ CodageCarte.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PROJET_CSHARP
+{
+    class CodageCarte
+    {
+        public CodageCarte(Carte carte)
+        {
+            //97 et 122
+
+        }
+    }
+}
